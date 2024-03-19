06e34198b7e5b523b451f8f2c5a12b2f2ef76140
LloydElery <dennisgooglar@gmail.com>
2024-03-19 15:39:42 +0100

    Lecture notes and Codealong for the 'auth' user and login

--- RecipeApp/src/app/app.component.ts
+++ RecipeApp/src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { LoginDetails } from './interfaces/login-details';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'RecipeApp';
+
+  loginDetails: LoginDetails;
+
+  user: User;
+
+  constructor(private auth: AuthService) {
+    this.loginDetails = {
+      email: 'den@den.den',
+      password: 'dendenden',
+    };
+
+    this.user = {
+      id: 1,
+      name: 'Dennis',
+      email: '',
+    };
+
+    auth.loginUser(this.loginDetails);
+  }
+
+  getUser() {
+    this.auth.getUser2().subscribe((res) => {
+      console.log(res[0]); // Tar ut det första elementet ur arrayen
+      this.user = res[0];
+    });
+  }
 }
---
+++ RecipeApp/src/app/services/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { LoginDetails } from '../interfaces/login-details';
+import { Observable, catchError, throwError } from 'rxjs';
+import { User } from '../interfaces/user';
+
+interface ResultData {
+  token: string
+} 
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class AuthService {
+
+  private baseUrl = 'http://127.0.0.1:8000/api/';
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  }
+
+  constructor(private http:HttpClient) { }
+
+  loginUser(loginDetails: LoginDetails) {
+    this.http
+      .post<ResultData>(this.baseUrl + 'login', loginDetails, this.httpOptions)
+      .pipe(catchError(this.handleError))
+      .subscribe(result => {
+        this.httpOptions.headers = this.httpOptions.headers.set('Authorization', "Bearer " + result.token);
+      })
+  }
+
+  getUser2(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl+'getuser/2', this.httpOptions);
+  }
+
+  private handleError(error: HttpErrorResponse){
+    if (error.status === 404) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+}
