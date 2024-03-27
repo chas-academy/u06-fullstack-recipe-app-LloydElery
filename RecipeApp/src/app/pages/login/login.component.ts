b554d37e49fb817fd3c80e28d580e1dad1d18f5b
LloydElery <dennisgooglar@gmail.com>
2024-03-27 15:42:02 +0100

    Backend Api - Laravel Connection and API Edamam connection!

--- RecipeApp/src/app/pages/login/login.component.ts
+++ RecipeApp/src/app/pages/login/login.component.ts
@@ -7,9 +7,6 @@ import {
   FormsModule,
   ReactiveFormsModule,
 } from '@angular/forms';
-import { User } from '../../interfaces/user';
-import { LoggedInUser } from '../../interfaces/logged-in-user';
-import { RegisterDetails } from '../../interfaces/register-details';
 @Component({
   selector: 'app-login',
   standalone: true,
