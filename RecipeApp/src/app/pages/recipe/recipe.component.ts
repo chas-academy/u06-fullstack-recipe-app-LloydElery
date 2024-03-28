802487182fc5804512d6f14a6871bb10437704fc
LloydElery <dennisgooglar@gmail.com>
2024-03-28 09:15:19 +0100

    Single recipe search - start on main

--- RecipeApp/src/app/pages/recipe/recipe.component.ts
+++ RecipeApp/src/app/pages/recipe/recipe.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { RecipeService } from '../../services/recipe.service';
+import { RecipeIdFormatterPipe } from '../../pipes/recipe-id-formatter.pipe';
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [],
+  imports: [RecipeIdFormatterPipe],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css',
 })
 export class RecipeComponent implements OnInit {
   id?: string;
 
-  constructor(private route: ActivatedRoute) {}
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
-  ngOninit(): void {
+  ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.id = String(params.get('id'));
+      console.log(this.id);
     });
   }
 }
