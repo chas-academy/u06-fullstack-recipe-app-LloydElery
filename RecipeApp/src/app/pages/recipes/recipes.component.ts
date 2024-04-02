96b3a2228b05aa4213a3f0eada5882eef5945465
LloydElery <dennisgooglar@gmail.com>
2024-04-02 16:06:45 +0200

    SearchForm and filtering by mealtype and cuisinetype

--- RecipeApp/src/app/pages/recipes/recipes.component.ts
+++ RecipeApp/src/app/pages/recipes/recipes.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Recipe } from '../../interfaces/recipe';
 import { RouterLink } from '@angular/router';
 import { RecipeIdFormatterPipe } from '../../pipes/recipe-id-formatter.pipe';
 import { NgOptimizedImage } from '@angular/common';
-
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [FormsModule, RouterLink, RecipeIdFormatterPipe, NgOptimizedImage],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+    RecipeIdFormatterPipe,
+    NgOptimizedImage,
+  ],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css',
 })
@@ -17,41 +27,75 @@ export class RecipesComponent {
   recipes?: Recipe[];
 
   searchterm = ''; // User search-input: string
-
-  cuisineType = ['American', 'Asian', 'British']; //TODO
-
-  mealType = ['Breakfast', 'Dinner', 'Lunch', 'Snack', 'Teatime']; //TODO
+  cuisineTypes = [
+    'American',
+    'Asian',
+    'British',
+    'Caribbean',
+    'Central Europe',
+    'Chinese',
+    'Eastern Europe',
+    'French',
+    'Indian',
+    'Italian',
+    'Japanese',
+    'Kosher',
+    'Mediterranean',
+    'Middle Eastern',
+    'Nordic',
+    'South American',
+    'South East Asian',
+  ];
+  mealTypes = ['Breakfast', 'Dinner', 'Lunch', 'Snack', 'Teatime'];
 
   constructor(private recipeService: RecipeService) {}
 
-  searchRecipe() {
-    this.recipeService.getRecipes(this.searchterm).subscribe((result) => {
-      console.table(result);
+  searchForm = new FormGroup({
+    searchterm: new FormControl(''),
+    mealType: new FormControl(''),
+    cuisineType: new FormControl(''),
+  });
 
-      let recipes: Recipe[];
-      recipes = result.hits.map(
-        (item: {
-          recipe: {
-            label: any;
-            image: any;
-            ingredientLines: any;
-            totalTime: any;
-            yield: any;
-          };
-          _links: { self: { href: any } };
-        }) => {
-          return {
-            label: item.recipe.label,
-            image: item.recipe.image,
-            ingredientLines: item.recipe.ingredientLines,
-            totalTime: item.recipe.totalTime,
-            yield: item.recipe.yield,
-            self: item._links.self.href,
-          };
-        }
-      );
-      console.log(recipes);
-      this.recipes = recipes;
-    });
+  searchData: any;
+
+  searchRecipe() {
+    this.searchData = this.searchForm.value;
+    let searchterm = this.searchData.searchterm;
+    let cuisineType = this.searchData.cuisineType;
+    let mealType = this.searchData.mealType;
+
+    console.log(this.searchData);
+    this.recipeService
+      .getRecipes(searchterm, cuisineType, mealType)
+      .subscribe((result) => {
+        console.table(result);
+
+        let recipes: Recipe[];
+        recipes = result.hits.map(
+          (item: {
+            recipe: {
+              label: any;
+              image: any;
+              ingredientLines: any;
+              totalTime: any;
+              yield: any;
+              mealType?: any;
+            };
+            _links: { self: { href: any } };
+          }) => {
+            return {
+              label: item.recipe.label,
+              image: item.recipe.image,
+              ingredientLines: item.recipe.ingredientLines,
+              totalTime: item.recipe.totalTime,
+              yield: item.recipe.yield,
+              melType: item.recipe.mealType,
+              self: item._links.self.href,
+            };
+          }
+        );
+        console.log(recipes);
+        this.recipes = recipes;
+      });
   }
 }
